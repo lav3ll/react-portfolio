49e404d27ff433f77ef14dd7bd416f69ca78af7a
lav3ll <lavell.francis@hotmail.com>
2024-02-03 14:54:56 +0000

    project.jsx and projects.jsx updated wuth base code

--- portfolio/src/components/Projects/Project.jsx
+++ portfolio/src/components/Projects/Project.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+      <div class="card-body p-0">
+        <div class="d-flex align-items-center">
+          <div class="p-5">
+            <h2 class="fw-bolder"></h2>
+            <p></p>
+          </div>
+          <img class="img-fluid" src="" alt="..." />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import projects from "../Data/Data.json";
+import Project from "./Project";
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    /* Projects Section*/
+    <section class="py-5">
+      <div class="container px-5 mb-5">
+        <div class="text-center mb-5">
+          <h1 class="display-5 fw-bolder mb-0">
+            <span class="text-gradient d-inline">Projects</span>
+          </h1>
+        </div>
+        <div class="row gx-5 justify-content-center">
+          <div class="col-lg-11 col-xl-9 col-xxl-8">
+            {projects.map((project) => {
+              return (
+                <Project
+                  image={project.image}
+                  title={project.title}
+                  language={project.language}
+                  github={project.github}
+                  demo={project.demo}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
