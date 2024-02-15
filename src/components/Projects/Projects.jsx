fad3071596f87bcc4f7aed5955bdf4adffd16092
lav3ll <lavell.francis@hotmail.com>
2024-02-15 22:28:58 +0000

    moved project images to public folder

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import data from "../Data/Data.json";
-import Project from "./Project";
+import React from 'react';
+import data from '../Data/Data.json';
+import Project from './Project';
 
 const Projects = () => {
   return (
     /* Projects Section*/
-    <section className="py-5">
-      <div className="container px-5 mb-5">
-        <div className="text-center mb-5">
-          <h1 className="display-5 fw-bolder mb-0">
-            <span className="text-gradient d-inline">Projects</span>
+    <section className='py-5'>
+      <div className='container px-5 mb-5'>
+        <div className='text-center mb-5'>
+          <h1 className='display-5 fw-bolder mb-0'>
+            <span className='text-gradient d-inline'>Projects</span>
           </h1>
         </div>
-        <div className="row gx-5 justify-content-center">
-          <div className="col-lg-11 col-xl-9 col-xxl-8">
+        <div className='row gx-5 justify-content-center'>
+          <div className='col-lg-11 col-xl-9 col-xxl-8'>
             {data.map((project) => (
               <Project
                 key={project.id}
